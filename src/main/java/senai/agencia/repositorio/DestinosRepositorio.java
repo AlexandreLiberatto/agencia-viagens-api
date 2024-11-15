f080a2e07b883e77fc8e7a59d1e222ba9c0bf8f7
unknown <alexandreliberatto@gmail.com>
2024-11-15 11:59:45 -0300

    Criando interface DestinosRepositorio
    Signed-off-by: unknown <alexandreliberatto@gmail.com>

--- src/main/java/senai/agencia/repositorio/DestinosRepositorio.java
+++ src/main/java/senai/agencia/repositorio/DestinosRepositorio.java
@@ -1,4 +1,7 @@
 package senai.agencia.repositorio;
 
-public interface DestinosRepositorio {
+import org.springframework.data.jpa.repository.JpaRepository; // JpaRepositori tras os métodos de CRUD integrados
+import senai.agencia.entidade.Destinos;
+
+public interface DestinosRepositorio extends JpaRepository<Destinos, Long> {
 }
