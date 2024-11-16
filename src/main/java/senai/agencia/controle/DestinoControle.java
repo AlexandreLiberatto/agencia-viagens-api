18519a9827a340cceb7a3869f61a6a94cdbd47ad
Alexandre liberato <alexandreliberatto@gmail.com>
2024-11-16 10:29:36 -0300

    Resolvendo conflitos
    Signed-off-by: Alexandre liberato <alexandreliberatto@gmail.com>

--- src/main/java/senai/agencia/controle/DestinoControle.java
+++ src/main/java/senai/agencia/controle/DestinoControle.java
@@ -1,13 +1,12 @@
 package senai.agencia.controle;
 
-<<<<<<< HEAD
+
 import org.springframework.web.bind.annotation.*;
 import senai.agencia.dto.DestinosDTO;
 import senai.agencia.servico.DestinosServico;
 
 import java.util.List;
 
-=======
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,7 +14,6 @@ import org.springframework.web.bind.annotation.RestController;
 import senai.agencia.dto.DestinosDTO;
 import senai.agencia.servico.DestinosServico;
 
->>>>>>> eb3f97e48eca269946a7c87a9c9bd936bd0232b3
 @RestController //indica para o Spring que é um controller
 @RequestMapping("/destinos") // End point para acessar está classe no navegador ex: localhost:8080/destinos
 public class DestinoControle {
@@ -31,12 +29,9 @@ public class DestinoControle {
         destinosServico.cadastrarDestino(dto);
     }
 
-<<<<<<< HEAD
     @GetMapping
     public List<DestinosDTO> listarDestinos(){
         return destinosServico.listarDestinos();
     }
 
-=======
->>>>>>> eb3f97e48eca269946a7c87a9c9bd936bd0232b3
 }
--- src/main/java/senai/agencia/servico/DestinosServico.java
+++ src/main/java/senai/agencia/servico/DestinosServico.java
@@ -6,12 +6,9 @@ import senai.agencia.dto.DestinosDTO;
 import senai.agencia.entidade.Destinos;
 import senai.agencia.repositorio.DestinosRepositorio;
 
-<<<<<<< HEAD
 import java.util.List;
 import java.util.stream.Collectors;
 
-=======
->>>>>>> eb3f97e48eca269946a7c87a9c9bd936bd0232b3
 @Service // Informa para o Spring que é uma classe de serviços
 public class DestinosServico {
 
@@ -30,11 +27,9 @@ public class DestinosServico {
         destinosRepositorio.save(destinos);
         return modelMapper.map(destinos, DestinosDTO.class);
     }
-<<<<<<< HEAD
 
     public List<DestinosDTO> listarDestinos() {
         return destinosRepositorio.findAll().stream().map(p -> modelMapper.map(p, DestinosDTO.class)).collect(Collectors.toList());
     }
-=======
->>>>>>> eb3f97e48eca269946a7c87a9c9bd936bd0232b3
+
 }
