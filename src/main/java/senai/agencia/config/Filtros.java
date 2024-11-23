63f26ab5bd93680b1e77b595288c2a3359428289
unknown <alexandreliberatto@gmail.com>
2024-11-23 18:58:41 -0300

    Finalizando configuraç~es de segurança, token criptografado
    Signed-off-by: unknown <alexandreliberatto@gmail.com>

--- src/main/java/senai/agencia/config/Filtros.java
+++ src/main/java/senai/agencia/config/Filtros.java
@@ -5,8 +5,11 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import senai.agencia.usuario.UsuarioRepositorio;
 
 import java.io.IOException;
 
@@ -16,20 +19,29 @@ public class Filtros extends OncePerRequestFilter {
 
     private final TokenServico tokenServico;
 
+    private final UsuarioRepositorio usuarioRepositorio;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
         String token = buscarToken(request);
-        String usuarioLogin = tokenServico.buscarUsuarioToken(token);
+        if(token != null){
+            var usuarioLogin = tokenServico.buscarUsuarioToken(token);
+            var usuario = usuarioRepositorio.findByLogin(usuarioLogin);
+
+            var autenticador = new UsernamePasswordAuthenticationToken(usuario, null, usuario.getAuthorities());
+
+            SecurityContextHolder.getContext().setAuthentication(autenticador);
+        }
 
         filterChain.doFilter(request, response);
     }
 
     private String buscarToken(HttpServletRequest request){
         var authorization = request.getHeader("Authorization");
-        if(authorization == null){
-            throw new RuntimeException("Token não encontrado!");
+        if(authorization != null){
+            return authorization.replace("Bearer ", "");
         }
-        return authorization.replace("Bearer ", "");
+        return null;
     }
 }
--- src/main/java/senai/agencia/config/Seguranca.java
+++ src/main/java/senai/agencia/config/Seguranca.java
@@ -1,7 +1,9 @@
 package senai.agencia.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,15 +12,22 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class Seguranca {
+
+    private final Filtros filtros;
 
     @Bean
     public SecurityFilterChain filtroSeguranca(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth.requestMatchers(HttpMethod.POST, "/login").permitAll()
+                        .anyRequest().authenticated())
+                        .addFilterBefore(filtros, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
